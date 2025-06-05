30030c5cf5f18250e43e1b936bdd31057ec38a9b
Aryan Kataria <aryankataria2023@gmail.com>
2025-06-05 12:24:58 -0500

    Tested home and log screens

---
+++ screens/LogScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LogScreen() {
+  return (
+    <View>
+      <Text>Welcome to the Log Screen!</Text>
+    </View>
+  );
+}
