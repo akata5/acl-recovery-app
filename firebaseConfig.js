ac98a8dda2a8f37be902d11f24ed9d280c3faa10
Aryan Kataria <aryankataria2023@gmail.com>
2025-06-08 18:18:23 -0500

    Safeguard api keys from firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzjVeQj1hIel-Jbp6nFwu7vFI_pIz9mpU",
-  authDomain: "acl-recovery-tracker.firebaseapp.com",
-  projectId: "acl-recovery-tracker",
-  storageBucket: "acl-recovery-tracker.firebasestorage.app",
-  messagingSenderId: "254838271577",
-  appId: "1:254838271577:web:7221d9a2b1dc581fd00b48",
-  measurementId: "G-N3TS5CLWEZ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
